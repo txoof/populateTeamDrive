0af5c88108b232f103eb1a826217cefd819812af
txoof <aaron.ciuffo@gmail.com>
2018-09-07 14:42:18 +0200

    update all methods for team drive support

--- gdrive/gdrive.py
+++ gdrive/gdrive.py
@@ -2,7 +2,8 @@
 
 # coding: utf-8
 
-# In[39]:
+# In[ ]:
+
 
 import logging
 import oauth2client
@@ -12,14 +13,18 @@ from apiclient import discovery
 from apiclient import errors 
 
 
-# In[66]:
+# In[ ]:
+
 
 class GDriveError(Exception):
     pass
 
 
-# In[81]:
+# In[163]:
 
+
+# google documentation here:
+# https://developers.google.com/apis-explorer/#p/
 class googledrive():
     '''
     creates a google drive interface object
@@ -87,8 +92,9 @@ class googledrive():
     def add(self, name = None, mimeType = False, parents = None, 
             fields = 'webViewLink, mimeType, id', sanitize = True):
         '''
-        add a file to google drive:
-        
+        add a file to google drive or team drive:
+        NB! when adding to the root of a team drive use the drive ID as the parent
+
         args:
             name (string): human readable name
             mimeType (string): mimeType (see self.mimeTypes for a complete list)
@@ -137,80 +143,11 @@ class googledrive():
         apiString = 'body={}, fields={}'.format(body, ','.join(fieldsProcessed))
         logging.debug('api call: files().create({})'.format(apiString))
         try:
-            result = self.service.files().create(body=body, fields=','.join(fieldsProcessed)).execute()
+            result = self.service.files().create(supportsTeamDrives=True, body=body, fields=','.join(fieldsProcessed)).execute()
             return(result)
         except errors.HttpError as e:
             raise GDriveError(e)
-            return(False)
-        
-        
-        #body = {'name':'release the schmoo!', 'mimeType':'application/vnd.google-apps.folder', 'parents':["0BzC-V2QIsGRGWXNxNmhjc0FITDQ"]}
-# service.files().create(body=body).execute()
-        
-
-    
-    def Xsearch(self, name = None, trashed = None, mimeType = False, fuzzy = False, date = None, dopperator = '>', 
-               parents = None, orderBy = 'createdTime', quiet = True):
-        '''
-        search for an item by name and other properties in google drive
-        
-        args:
-            name (string): item name in google drive - required
-            trashed (bool): item is not in trash - default None (not used)
-            mimeType = (string): item is one of the known mime types (gdrive.mimeTypes) - default None
-            fuzzy = (bool): substring search of names in drive
-            date = (RFC3339 date string): modification time date string (YYYY-MM-DD)
-            dopperator (date comparison opprator string): <, >, =, >=, <=  - default >
-            parents = (string): google drive file id string
-            orderBy = (comma separated string): order results assending by keys below - default createdTime:
-                        'createdTime', 'folder', 'modifiedByMeTime', 
-                        'modifiedTime', 'name', 'quotaBytesUsed', 
-                        'recency', 'sharedWithMeTime', 'starred', 
-                        'viewedByMeTime'
-            fields (comma separated string): properties to query and return any of the following:
-                'parents', 'mimeType', 'webViewLink', 
-                'size', 'createdTime', 'trashed'
-                'id'
-            sanitize (bool): remove any field options that are not in the above list - false to allow anything
-                        
-                        
-            
-        returns:
-            list of file dict
-        '''
-        features = ['name', 'trashed', 'mimeType', 'date', 'parents']
-        build = {'name' : 'name {} "{}"'.format(('contains' if fuzzy else '='), name),
-                 'trashed' : 'trashed={}'. format(trashed),
-                 'mimeType' : 'mimeType="{}"'.format(self.mimeTypes[mimeType] if mimeType in self.mimeTypes else ''),
-                 'parents': '"{}" in parents'.format(parents),
-                 'date': 'modifiedTime{}"{}"'.format(dopperator, date)}
-
-        
-        # provides for setting trashed to True/False if the input is not None
-        if not isinstance(trashed, type(None)):
-            # set to true as the variable is now in use, but it's value has been set above
-            trashed = True
-        
-        qList = []
-
-        # evaluate feature options; if they are != None/False, use them in building query
-        for each in features:
-            if eval(each):
-                qList.append(build[each])
-                
-        if not quiet:
-            print(' and '.join(qList))
-        
-        apiString = 'q={}, orderBy={})'.format(' and '.join(qList), orderBy)
-        logging.debug('apicall: files().list({})'.format(apiString))
-        try:
-            # build a query with "and" statements
-
-            result = self.service.files().list(q=' and '.join(qList), orderBy=orderBy).execute()
-            return(result)
-        except errors.HttpError as e:
-            raise GDriveError(e)
-            return(False)
+            return(False)        
         
     def search(self, name = None, trashed = None, mimeType = False, fuzzy = False, date = None, dopperator = '>', 
                parents = None, orderBy = 'createdTime', teamdrive = None, quiet = True):
@@ -347,14 +284,26 @@ class googledrive():
         apiString = 'fileId={}, fields={}'.format(fileId, ','.join(fieldsProcessed))
         logging.debug('files().get({})'.format(apiString))
         try:
-            result = self.service.files().get(fileId=fileId, fields=','.join(fieldsProcessed)).execute()
+            result = self.service.files().get(supportsTeamDrives=True, fileId=fileId, fields=','.join(fieldsProcessed)).execute()
             return(result)
         except errors.HttpError as e:
             raise GDriveError(e)
             return(False)
         
-
+    def getpermissions(self, fileID):
+        """
+        get a file, folder or Team Drive's permissions
+        """
+        try:
+            permissions = self.service.permissions().list(fileId=fileID, 
+                                                          supportsTeamDrives=True).execute()
+            return permissions
+        except errors.HttpError, error:
+              print 'An error occurred: %s' % error
+        return None
+        
     def parents(self, fileId):
+        # need to update to work with TeamDrive
         """get a file's parents.
 
         Args:
@@ -369,10 +318,24 @@ class googledrive():
             return(parents)
         except errors.HttpError as e:
             raise GDriveError(e)
-            return(False)
+            return(None)
     
     def rm(self):
         pass
+    
+    def getuserid(self):
+        '''
+        get the current user's ID
+        '''
+        # more info here: https://developers.google.com/apis-explorer/#search/drive.about.get/m/drive/v3/drive.about.get?fields=user&_h=1&
+        try:
+            uid = self.service.about().get(fields="user/permissionId").execute()
+            #return(uid)
+            return(uid['user']['permissionId'])
+        except errors.HttpError as e:
+            raise GDriveError(e)
+            return(None)
+        
     
     def listTeamDrives(self):
         '''
@@ -381,12 +344,39 @@ class googledrive():
             raises GDriveError
             
             returns: 
-                dictonary of first page of TeamDrives
+                dictonary of first page of TeamDrives and capabilities
         '''
+        fields = ['teamDrives']
+       
+        
         try:
-            result = self.service.teamdrives().list().execute()
+            result = self.service.teamdrives().list(fields=','.join(fields)).execute()
             return(result['teamDrives'])
         except errors.HttpError as e:
             raise GDriveError(e)
             return(False)
+
+
+# In[167]:
+
+
+# create an instance for testing
+# from auth import *
+# credential_store = "/tmp/"
+# credentials = getCredentials(credential_store)
+# myDrive = googledrive(credentials)
+
+
+
+
+# In[180]:
+
+
+
+
+
+# In[166]:
+
+
+myDrive.getprops("0ACLfU8KeD_BHUk9PVA")
 
