3af112f13cb76ad764abf2a153e3f688166c9d5c
txoof <aaron.ciuffo@gmail.com>
2018-10-07 21:38:07 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env ipython
+
+# coding: utf-8
+
+# In[6]:
+
+from setuptools import setup
+
+
+# In[9]:
+
+setup(name='portfolioCreator', 
+      version='0.1', 
+      description='Create student portfolio folders on Google Team Drive', 
+      url='https://github.com/txoof/populateTeamDrive', 
+      author='Aaron Ciuffo',
+      author_email='aaron.ciuffo at gmail',
+      license='GPLv3',
+      packages=['portfolioCreator'],
+      zip_safe=False)
+
