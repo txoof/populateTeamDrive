1455d560837754ef2b7e4b445edee8e28f9ee171
txoof <aaron.ciuffo@gmail.com>
2018-10-07 22:37:56 +0200

    reorganize resources

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env ipython
-
-# coding: utf-8
-
-# In[6]:
-
 from setuptools import setup
-
-
-# In[9]:
-
 setup(name='portfolioCreator', 
       version='0.1', 
       description='Create student portfolio folders on Google Team Drive', 
@@ -17,5 +8,6 @@ setup(name='portfolioCreator',
       author_email='aaron.ciuffo at gmail',
       license='GPLv3',
       packages=['portfolioCreator'],
+      include_package_data=True,
       zip_safe=False)
 
