e3c8807bef5438df835ebc0a8b68b6f705e65f6f
txoof <aaron.ciuffo@gmail.com>
2018-10-15 16:05:20 +0200

    update build script to create zip file

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [[ "$VIRTUAL_ENV" != "" ]] ; then
   pyinstaller --clean *.spec
-  cp ./dist/portfolioCreator /Volumes/GoogleDrive/Team\ Drives/ES\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/
+  zip -j /Volumes/GoogleDrive/Team\ Drives/ES\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/portfolioCreator.zip ./dist/portfolioCreator
+#  cp ./dist/portfolioCreator /Volumes/GoogleDrive/Team\ Drives/ES\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/
 else
   echo "This must be run from within a virtual environment with pyinstaller"
 fi
