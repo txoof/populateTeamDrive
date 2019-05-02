b828d11728457c488e85d6203ba9fae912af56a1
txoof <aaron.ciuffo@gmail.com>
2019-05-02 16:57:44 +0200

    update for pipenv

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 APPNAME="portfolioCreator"
 
-if [[ "$VIRTUAL_ENV" != "" ]] ; then
-  ~/bin/nbconvert "$APPNAME".ipynb
-  pyinstaller --clean *.spec
-  zip -j /Volumes/GoogleDrive/Team\ Drives/ES\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/portfolioCreator.zip ./dist/portfolioCreator
+# uneeded when running with pipenv
+#if [[ "$VIRTUAL_ENV" != "" ]] ; then
+#  ~/bin/nbconvert "$APPNAME".ipynb
+
+pipenv run  pyinstaller --clean *.spec
+zip -j /Volumes/GoogleDrive/Team\ Drives/ES\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/portfolioCreator.zip ./dist/portfolioCreator
 #  cp ./dist/portfolioCreator /Volumes/GoogleDrive/Team\ Drives/ES\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/
-else
-  echo "This must be run from within a virtual environment with pyinstaller"
-fi
+#else
+#  echo "This must be run from within a virtual environment with pyinstaller"
+#fi
