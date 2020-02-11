cd2fec76e23ae0274d5fa97f68fcf2c8402e60c7
txoof <aaron.ciuffo@gmail.com>
2020-02-11 09:56:00 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 APPNAME="portfolioCreator"
 DATESTR=`date +%F_%H.%M`
+FNAME="$APPNAME-$DATESTR.zip"
+LNAME="$APPNAME-latest.zip"
 
-#pipenv run  pyinstaller --clean *.spec
-mv /Volumes/GoogleDrive/Shared\ drives/ASH\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/*.zip /Volumes/GoogleDrive/Team\ Drives/ASH\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/Old\ Versions
-zip -j /Volumes/GoogleDrive/Team\ Drives/ASH\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/portfolioCreator_$DATESTR.zip ./dist/portfolioCreator
+
+pipenv run  pyinstaller --clean *.spec
+zip -j $FNAME ./dist/portfolioCreator
+cp  $FNAME $LNAME
+
+git add $FNAME $LNAME
+git commit -m "update build" $FNAME $LNAME
+git push
+
+
+#mv /Volumes/GoogleDrive/Shared\ drives/ASH\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/*.zip /Volumes/GoogleDrive/Team\ Drives/ASH\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/Old\ Versions
+
+#zip -j /Volumes/GoogleDrive/Team\ Drives/ASH\ Student\ Cumulative\ Folders/PortfolioCreator\ Application/portfolioCreator_$DATESTR.zip ./dist/portfolioCreator
 
